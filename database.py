a586dda1353763ad57232a98c9ecfa89d131fe94
Yasnyy Ilya <fastbeka@gmail.com>
2024-12-10 20:51:53 +0500

    Проверка декоратора подключения БД

--- database.py
+++ database.py
@@ -23,14 +23,30 @@ def connect():
     return conn, cur
 
 
-def disconnect(conn):
+def disconnect(conn, cur):
     conn.commit()
+    cur.close()
     conn.close()
 
 
-def search_in_database(chat_id):
-    conn, cur = connect()
-    cur.execute(f"SELECT user_choice FROM users WHERE chat_id = {chat_id}")
+def db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn, cur = connect()
+        if not conn:
+            print("Database connection failed.")
+            return None
+        try:
+            result = func(*args, cur=cur, conn=conn, **kwargs)
+        finally:
+            disconnect(conn, cur)
+        return result
+    return wrapper
+
+
+@db_connection
+def search_in_database(chat_id, cur):
+    QUERY = f"SELECT user_choice FROM users WHERE chat_id = {chat_id}"
+    cur.execute(QUERY)
     user_choice = cur.fetchone()
     if user_choice is None:
         isFind = False
@@ -38,12 +54,11 @@ def search_in_database(chat_id):
     else:
         isFind = True
         user_choice = user_choice[0]
-    disconnect(conn)
     return isFind, user_choice
 
 
-def write_new_user(chat_id, user_name, user_choice):
-    conn, cur = connect()
+@db_connection
+def write_new_user(chat_id, user_name, user_choice, cur):
     cur.execute(f"SELECT MAX(id) FROM users")
     max_id = cur.fetchone()[0]
     if max_id is None:
@@ -55,19 +70,17 @@ def write_new_user(chat_id, user_name, user_choice):
         "INSERT INTO users (id, chat_id, user_name, user_choice, create_date) VALUES (?, ?, ?, ?, ?)",
         (user_id, chat_id, user_name, user_choice, str(time))
     )
-    conn.commit()
     content = str(http_requests.create_content(user_choice))
     cur.execute(
         "INSERT INTO users_content (id, chat_id, content, time) VALUES (?, ?, ?, ?)",
         (user_id, chat_id, content, time)
     )
-    disconnect(conn)
 
 
-def rewrite_content_user(chat_id, content):
-    conn, cur = connect()
-    time = str(datetime.now())
+@db_connection
+def rewrite_content_user(chat_id, content, cur):
     try:
+        time = str(datetime.now())
         cur.execute(
             """
             UPDATE users_content SET content = ?, time = ? where chat_id = ?
@@ -76,52 +89,44 @@ def rewrite_content_user(chat_id, content):
         )
     except mariadb.ProgrammingError as err:
         print(err)
-    disconnect(conn)
 
 
-def remove_user(chat_id):
-    conn, cur = connect()
+@db_connection
+def remove_user(chat_id, cur):
     try:
         cur.execute(f"DELETE FROM users WHERE chat_id={chat_id}")
         cur.execute(f"DELETE FROM users_content WHERE chat_id={chat_id}")
-        disconnect(conn)
     except mariadb.Error as err:
         print(f"Error: {err}")
 
 
-def all_users():
-    conn, cur = connect()
-    if conn != 0:
+@db_connection
+def all_users(cur):
+    try:
         cur.execute("SELECT chat_id, user_choice FROM users")
         users = cur.fetchall()
         cur.execute("SELECT content FROM users_content")
-        try:
-            contents = cur.fetchall()
-        except IndexError:
-            return 1
+        contents = cur.fetchall()
         users_contents = []
         for i in range(0, len(users)):
             users_contents.append([users[i], contents[i]])
-        disconnect(conn)
         return users_contents
-    else:
+    except IndexError as e:
+        print(f"Ошибка {e}")
         return 1
 
 
-def test_user():
-    conn, cur = connect()
-    if conn != 0:
+@db_connection
+def test_user(cur):
+    try:
         cur.execute("SELECT chat_id, user_choice FROM users WHERE chat_id='558710006'")
         users = cur.fetchall()
         cur.execute("SELECT content FROM users_content WHERE chat_id='558710006'")
-        try:
-            contents = cur.fetchall()
-        except IndexError:
-            return 1
+        contents = cur.fetchall()
         users_contents = []
         for i in range(0, len(users)):
             users_contents.append([users[i], contents[i]])
-        disconnect(conn)
         return users_contents
-    else:
+    except IndexError as e:
+        print(f"Ошибка {e}")
         return 1
